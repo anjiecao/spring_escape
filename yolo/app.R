233667a95e40cf2702f4bff3701790c4406b22f4
anjie cao <anjiecao@stanford.edu>
2021-03-08 14:14:13 -0800

    filter by starting date range; interactive table

--- yolo/app.R
+++ yolo/app.R
@@ -5,10 +5,13 @@ library(here)
 library(tidyverse)
 PATH <- here("yolo.csv")
 
-df.accomodations <- read_csv(PATH)
+df.accomodations <- read_csv(PATH) %>% 
+  mutate(available_start = as.Date(available_start, "%m/%d/%Y"), 
+         available_end = as.Date(available_end, "%m/%d/%Y"))
 
 ui <- fluidPage(
   h1("Yolo"),
+  h2(paste0("last update:", format(Sys.time(), "%a %b %d %X %Y"))), 
   selectInput("housing_name", "housing_name",
               choices = c("All", df.accomodations$housing_name)),
   
@@ -19,11 +22,14 @@ ui <- fluidPage(
                             selected = df.accomodations$dryer
          ),
   
+
+  dateRangeInput("date_range", h3("Starting Date Range")),
+  
   numericInput("people_count", 
                h3("Number Of People"), 
                value = 1),
     # Replace the tableOutput() with DT's version
-  tableOutput("table")
+  DT::dataTableOutput("table")
 )
 
 server <- function(input, output) {
@@ -31,6 +37,7 @@ server <- function(input, output) {
     data <- df.accomodations
     data <- data %>% 
       filter(dryer %in% input$dryerSelect)
+    
     if (input$housing_name != "All") {
       data <- subset(
         data,
@@ -38,17 +45,27 @@ server <- function(input, output) {
       )
     }
     
+    # filter by person number
     data <- data %>% 
+      filter(max_n > input$people_count | max_n == input$people_count) %>% 
       mutate(
         price_per_person = price_month / input$people_count
       ) %>% 
       relocate(c(price_per_person, price_month), .after = "housing_name")
     
+    # filter by date 
+    data <- data %>% 
+      filter(available_start >= input$date_range[1] & 
+             available_start <= input$date_range[2])  
+    #         available_end <= input$date_range[2] &
+    #         available_end >= input$date_range[1])
+    
+    
     data 
   })
   
   # Replace the renderTable() with DT's version
-  output$table <- renderTable({
+  output$table <- DT::renderDataTable({
     data <- updated_data()
     data
   })
